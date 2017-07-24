dcab8d3b16c423c7a261a63c1f73dcf7a8542ebf
Kashyap Barua <xodiac642@gmail.com>
2017-07-24 13:31:37 +0530

    Random Forest with Custom TrainControl

--- Project.R
+++ Project.R
@@ -13,3 +13,27 @@ myControl <- trainControl(
   savePredictions = TRUE,
   index = myFolds
 )
+
+#Fitting the baseline model
+
+# Fit glmnet model: model_glmnet
+model_glmnet <- train(
+  x = churn_x, y = churn_y,
+  metric = "ROC",
+  method = "glmnet",
+  trControl = myControl
+)
+
+##OBSERVATION
+#Selecting tuning parameters
+#Fitting alpha = 0.55, lambda = 0.0182 on full training set
+
+#Random forest with custom trainControl
+
+# Fit random forest: model_rf
+model_rf <- train(
+  x = churn_x, y = churn_y,
+  metric = "ROC",
+  method = "ranger",
+  trControl = myControl
+)
